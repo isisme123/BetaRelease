a9297297fc824f541e68489c572c4e4102bf37f3
judereynolds <judereynolds100@gmail.com>
2023-04-03 14:56:18 -0800

    Not working but trying to make game state

---
+++ app/src/main/java/com/example/gofishgui/fish/fishGameState.java
@@ -0,0 +1,44 @@
+package com.example.gofishgui.fish;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class fishGameState {
+    // Instance variables
+    private int currentPlayer;
+
+    //public List<FishCard> deck;
+    public List<FishCard> humanHand; //Map of the player ID to their hand of cards
+    public List<FishCard> computerHand;
+    //private Map<Integer, Integer> playerScores; // ..scores?!
+    private int playerScore;
+    private int opponentScore;
+    private boolean gameOver;
+    private FishDeck deck;
+
+    public fishGameState(int currentPlayer, FishCard[] deck, int playerScore, int opponentScore, boolean gameOver) {
+
+
+        this.currentPlayer = currentPlayer;
+        this.playerScore = playerScore;
+        this.opponentScore = opponentScore;
+        this.gameOver = gameOver;
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void playGame() {
+
+    }
+
+    public void nextPlayer() {
+
+    }
+
+    public boolean isGamerOver() {
+
+    }
+}
