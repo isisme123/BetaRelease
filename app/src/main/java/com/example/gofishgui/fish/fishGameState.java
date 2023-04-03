a7df8f1cc2d9384025146f174f641f0bbe24809c
judereynolds <judereynolds100@gmail.com>
2023-04-03 15:01:09 -0800

    Not working but trying to make game state also pulled things

--- app/src/main/java/com/example/gofishgui/fish/fishGameState.java
+++ app/src/main/java/com/example/gofishgui/fish/fishGameState.java
@@ -7,18 +7,21 @@ public class fishGameState {
     // Instance variables
     private int currentPlayer;
 
-    //public List<FishCard> deck;
+    public List<FishCard> deck;
     public List<FishCard> humanHand; //Map of the player ID to their hand of cards
     public List<FishCard> computerHand;
     //private Map<Integer, Integer> playerScores; // ..scores?!
     private int playerScore;
     private int opponentScore;
     private boolean gameOver;
-    private FishDeck deck;
+    //private FishDeck deck;
 
-    public fishGameState(int currentPlayer, FishCard[] deck, int playerScore, int opponentScore, boolean gameOver) {
+    public fishGameState(int currentPlayer, List<FishCard> deck, List<FishCard> humanHand, List<FishCard> computerHand,
+                         int playerScore, int opponentScore, boolean gameOver) {
 
-
+        this.deck = deck;
+        this.humanHand = humanHand;
+        this.computerHand = computerHand;
         this.currentPlayer = currentPlayer;
         this.playerScore = playerScore;
         this.opponentScore = opponentScore;
@@ -41,4 +44,6 @@ public class fishGameState {
     public boolean isGamerOver() {
 
     }
+
+
 }
