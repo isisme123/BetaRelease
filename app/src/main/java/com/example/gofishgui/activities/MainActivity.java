f7f3096923a67028c6cfef84dab1a52c7e330001
21hph <hpham69032003@gmail.com>
2023-04-03 02:49:02 -0700

    Human Hand

--- app/src/main/java/com/example/gofishgui/activities/MainActivity.java
+++ app/src/main/java/com/example/gofishgui/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.gofishgui.R;
@@ -34,33 +35,49 @@ public class MainActivity extends AppCompatActivity {
         deck.shuffle(); // shuffles deck
         deck.dealCards(); // deals cards
         FishCard[] humanHand = deck.getHumanHand(); // gets human hand
+        FishCard[] computerHand = deck.getComputerHand(); // gets computer hand
         updateHandImages(humanHand, pc); // updates images for human hand
     }
 
     // Helper method that changes the images on the cards
     public void updateHandImages(FishCard[] hand, ImageView[] imageViews) {
-        for (int i = 0; i < hand.length; i++) {
-            FishCard card = hand[i];
-            String valueWord = "";
-            switch (card.getValue()) {
-                case 1: valueWord = "ace"; break;
-                case 2: valueWord = "two"; break;
-                case 3: valueWord = "three"; break;
-                case 4: valueWord = "four"; break;
-                case 5: valueWord = "five"; break;
-                case 6: valueWord = "six"; break;
-                case 7: valueWord = "seven"; break;
-                case 8: valueWord = "eight"; break;
-                case 9: valueWord = "nine"; break;
-                case 10: valueWord = "ten"; break;
-                case 11: valueWord = "jack"; break;
-                case 12: valueWord = "queen"; break;
-                case 13: valueWord = "king"; break;
-                default: valueWord = "unknown"; break;
+        for (int i = 0; i < hand.length && i < imageViews.length; i++) {
+            if (hand[i] != null) {
+                String fileName = "";
+                switch (hand[i].getValue()) {
+                    case 1: fileName += "ace"; break;
+                    case 2: fileName += "two"; break;
+                    case 3: fileName += "three"; break;
+                    case 4: fileName += "four"; break;
+                    case 5: fileName += "five"; break;
+                    case 6: fileName += "six"; break;
+                    case 7: fileName += "seven"; break;
+                    case 8: fileName += "eight"; break;
+                    case 9: fileName += "nine"; break;
+                    case 10: fileName += "ten"; break;
+                    case 11: fileName += "jack"; break;
+                    case 12: fileName += "queen"; break;
+                    case 13: fileName += "king"; break;
+                }
+
+                fileName += "_of_";
+
+                switch (hand[i].getRank()) {
+                    case "hearts": fileName += "hearts"; break;
+                    case "diamonds": fileName += "diamonds"; break;
+                    case "clubs": fileName += "clubs"; break;
+                    case "spades": fileName += "spades"; break;
+                }
+
+                int imageResource = getResources().getIdentifier(fileName, "drawable", getPackageName());
+                imageViews[i].setImageResource(imageResource);
+                imageViews[i].setVisibility(View.VISIBLE);
             }
-            String fileName = valueWord + "_of_" + card.getRank().toLowerCase();
-            int resourceId = getResources().getIdentifier(fileName, "drawable", getPackageName());
-            imageViews[i].setImageResource(resourceId);
+
+            else {
+                // hide the ImageView if there is no card to display
+                imageViews[i].setVisibility(View.INVISIBLE);
+            }
         }
     }
 }
